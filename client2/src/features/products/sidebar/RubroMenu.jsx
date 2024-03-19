20a1df75971ef105ad26b68b15131e00af1274aa
gabrieeelsp <gabrieeelsp@gmail.com>
2024-03-19 10:11:41 -0300

    Se agrego funcionalidad al sideBar, acordeon a los menus

--- client2/src/features/products/sidebar/RubroMenu.jsx
+++ client2/src/features/products/sidebar/RubroMenu.jsx
@@ -1,24 +1,49 @@
-import { Link } from 'react-router-dom'
+import { NavLink, useLocation } from 'react-router-dom'
 
 import SubrubroMenu from './SubrubroMenu'
 
 import slugify from 'slugify'
+import { useEffect, useState } from 'react';
 
 const RubroMenu = (props) => {
     const { rubro } = props;
+
+    const location = useLocation();
+
+    const [isOpen, setIsOpen] = useState(false);
+
+    useEffect(() => {
+        if ( location.pathname.split('/')[1].toLowerCase() === slugify(rubro.name, {lower: true, strict: true})) {
+            setIsOpen(true)
+        } else {
+            setIsOpen(false)
+        }
+    }, [location])
+
     return (
         <li>
-            <Link to={`/${slugify(rubro.name, {lower: true, strict: true})}`}>{rubro.name}</Link>
-
-            <ul>
-                {rubro.subrubros.map((subrubro) => (
-                    <SubrubroMenu 
-                        key={`subrubro-${subrubro.id}`} 
-                        subrubro={subrubro} 
-                        rubro={rubro}
-                        />
-                ))}
-            </ul>
+            <div className='flex justify-between'>
+                <NavLink 
+                    className={(navData) => navData.isActive ? 'text-purple-600' : ''}
+                    to={`/${slugify(rubro.name, {lower: true, strict: true})}`}
+                >{rubro.name}</NavLink>
+                <button onClick={() => setIsOpen(oldValue => !oldValue)} >
+                    { isOpen ? <span>-</span> : <span>+</span> }
+                </button>
+            </div>
+            
+            <div className={`grid overflow-hidden transition-all duration-100 ease-in-out ${isOpen ? 'grid-rows-[1fr] opacity-100' : 'grid-rows-[0fr] opacity-0'}`}>
+                <ul className='overflow-hidden'>
+                    {rubro.subrubros.map((subrubro) => (
+                        <SubrubroMenu 
+                            key={`subrubro-${subrubro.id}`} 
+                            subrubro={subrubro} 
+                            rubro={rubro}
+                            />
+                    ))}
+                </ul>
+            </div>
+            
         </li>
     )
 }
--- client2/src/features/products/sidebar/SubrubroMenu.jsx
+++ client2/src/features/products/sidebar/SubrubroMenu.jsx
@@ -1,4 +1,4 @@
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 import slugify from "slugify";
 
@@ -6,7 +6,10 @@ const RubroMenu = (props) => {
     const { subrubro, rubro } = props;
     return (
         <li className="pl-2">
-            <Link to={`/${slugify(rubro.name, {lower: true, strict: true})}/${slugify(subrubro.name, {lower: true, strict: true})}`}>{subrubro.name}</Link>
+            <NavLink 
+                className={(navData) => navData.isActive ? 'text-purple-500' : ''}
+                to={`/${slugify(rubro.name, {lower: true, strict: true})}/${slugify(subrubro.name, {lower: true, strict: true})}`}
+            >{subrubro.name}</NavLink>
         </li>
     )
 }
